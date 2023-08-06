a0faed7bcaa7707c6a99f2dce9c687dad12b079c
FENGTONGSHUAI <3226779785@qq.com>
2023-08-06 13:28:43 +0800

    TCPIP网络编程第5章

--- TCP_IP/TCP_IP_WLBC/chapter5/op_server.c
+++ TCP_IP/TCP_IP_WLBC/chapter5/op_server.c
@@ -6,14 +6,16 @@
 #include <sys/socket.h>
 
 #define BUF_SIZE 1024
+#define OPSZ 4
 void error_handling(char *message);
+int calculate(int opnum, int opnds[], char oprator);
 
 int main(int argc, char* argv[])
 {
     int serv_sock, clnt_sock;
-    char message[BUF_SIZE];
-    int str_len, i;
-
+    char opinfo[BUF_SIZE];
+    int result, opnd_cnt, i;
+    int recv_cnt, recv_len;
     struct sockaddr_in serv_adr, clnt_adr;
     socklen_t clnt_adr_sz;
 
@@ -49,27 +51,41 @@ int main(int argc, char* argv[])
 
     for(i = 0; i < 5; ++i)
     {
+        opnd_cnt = 0;
         clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
-        if(clnt_sock == -1) 
-        {
-            error_handling("accept() error");
-        }
-        else
-        {
-            printf("Connected client %d \n", i + 1);
-        }
+        read(clnt_sock, &opnd_cnt, 1);
 
-        while((str_len = read(clnt_sock, message, BUF_SIZE)) != 0)
+        recv_len = 0;
+        while((opnd_cnt * OPSZ + 1) > recv_len)
         {
-            write(clnt_sock, message, str_len);
+            recv_cnt = read(clnt_sock, &opinfo[recv_len], BUF_SIZE - 1);
+            recv_len += recv_cnt;
         }
-        printf("str_len = %d\n", str_len);
-
+        result = calculate(opnd_cnt, (int *)opinfo, opinfo[recv_len - 1]);
+        write(clnt_sock, (char*)&result, sizeof(result));
         close(clnt_sock);
     }
 
     close(serv_sock);
     return 0;
+}
+
+int calculate(int opnum, int opnds[], char op)
+{
+    int result = opnds[0], i;
+    switch(op)
+    {
+        case '+':
+            for(i = 1; i < opnum; ++i) result += opnds[i];
+            break;
+        case '-':
+            for(i = 1; i < opnum; ++i) result -= opnds[i];
+            break;
+        case '*':
+            for(i = 1; i < opnum; ++i) result *= opnds[i];
+            break;
+    }
+    return result;
 }
 
 void error_handling(char* message) 
@@ -79,4 +95,4 @@ void error_handling(char* message)
     exit(1);
 }
 
-// gcc -o echo_server echo_server.c
+// gcc -o op_server op_server.c
