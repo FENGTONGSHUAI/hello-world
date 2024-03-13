1f1a990c91fe938c6dc94c72956e9948927df9e2
FENGTONGSHUAI <3226779785@qq.com>
2024-03-13 23:52:33 +0800

    learn

---
+++ linkers_and_loaders/chapter10/foo.c
@@ -0,0 +1,6 @@
+int foo()
+{
+    return 123;
+}
+
+// gcc -S -g foo.c -o foo.s
--- project/webbench/webbench.c
+++ project/webbench/webbench.c
@@ -7,6 +7,41 @@
 #include <time.h>
 #include <signal.h>
 
+#define DEBUG           1
+
+/* globals */
+int http10 = 1; /* 0 - http/0.9, 1 - http/1.0, 2 - http/1.1 */
+
+/*  方法定义  Allow: GET, HEAD, OPTIONS, TRACE*/
+#define METHOD_GET 0
+#define METHOD_HEAD 1
+#define METHOD_OPTIONS 2
+#define METHOD_TRACE 3
+/* 软件版本 */
+#define PROGRAM_VERSION "1.5"
+
+int force = 0;
+int force_reload = 0;
+int benchtime = 30; // 测试时间
+
+static const struct option long_options[] = 
+{
+    {"force", no_argument, &force, 1},  // 不需要参数, 返回0, force变量置为1
+    {"reload", no_argument, &force_reload, 1},
+    {"time", required_argument, NULL, 't'}, // 需要参数, 返回t
+    {"help", no_argument, NULL, '?'}, // 不需要参数, 返回?
+    {"http09", no_argument, NULL, '9'},
+    {"http10", no_argument, NULL,'1'},
+    {"http11", no_argument,NULL, '2'},
+    {"get", no_argument, &method, METHOD_GET},
+    {"head", no_argument, &method, METHOD_HEAD},
+    {"options", no_argument, &method, METHOD_OPTIONS},
+    {"trace", no_argument, &method, METHOD_TRACE},
+    {"version", no_argument, NULL, 'V'},
+    {"proxy", required_argument, NULL, 'p'},
+    {"clients", required_argument, NULL, 'c'},
+    {NULL, 0, NULL, 0}  // 结尾
+}
 
 static void usage(void)
 {
@@ -40,6 +75,50 @@ int main(int argc, char *argv[])
         return 2;
     }
 
+    while(opt = getopt_long(argc, argv, "912Vfrt:p:c:?h", long_options, &options_index)) {
+        switch (opt)
+        {
+            // long_opt 且 option结构体的flag不为NULL时返回0
+            case 0:
+                break;
+            case 'f': force = 1; break;
+            case 'r': force_reload = 1; break;
+            case '9': http10 = 0; break;
+            case '1': http10 = 1; break;
+            case '2': http10 = 2; break;
+            case "V": printf(PROGRAM_VERSION"\n"); exit(0);
+            case 't': 
+                #ifdef DEBUG
+                    print("case 't' optarg = %s", optarg);
+                #endif
+                benchtime = atoi(optarg); 
+                break;
+            case 'p':
+                #ifdef DEBUG
+                    print("case 'p' optarg = %s", optarg);
+                #endif
+                /* proxy server parsing server:port */
+                tmp = strrchr(optarg, ':');
+                if(tmp == NULL)
+                {
+                    break;
+                }
+                if(tmp == optarg)
+                {
+                    fprintf(stderr, "Error in option --proxy %s: Missing hostname.\n", optarg);
+		            return 2;
+                }
+                if (tmp == (optarg + strlen(optarg) - 1))
+                {
+                    fprintf(stderr, "Error in option --proxy %s Port number is missing.\n", optarg);
+		            return 2;
+                }
+                
+            default:
+                break;
+        }
+    }
+    // 20240313暂时没有编译通过, 先提交, 后面完善
 
     return 0;
 }
