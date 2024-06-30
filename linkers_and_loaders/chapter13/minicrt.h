fff6141b35ffb3a67b34f2eb6e377b15af577321
FENGTONGSHUAI <3226779785@qq.com>
2024-06-30 22:32:08 +0800

    fix

--- linkers_and_loaders/chapter13/minicrt.h
+++ linkers_and_loaders/chapter13/minicrt.h
@@ -63,6 +63,6 @@ int atexit(atexit_func_t func);
 #endif
 
 /*
-gcc -c -fno-builtin -nostdlib -fno-stack-protector entry.c malloc.c stdio.c string.c printf.c
+gcc -c -ggdb -fno-builtin -nostdlib -fno-stack-protector entry.c malloc.c stdio.c string.c printf.c
 ar -rs minicrt.a malloc.o printf.o stdio.o string.o
 */
